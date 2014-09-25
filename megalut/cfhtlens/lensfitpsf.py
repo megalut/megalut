292d70494bf6b4eade4b079064584bddec910b54
mtewes <malte.tewes@gmail.com>
2014-09-25 13:20:16 +0200

    Stacking of exposure PSFs working

--- megalut/cfhtlens/lensfitpsf.py
+++ megalut/cfhtlens/lensfitpsf.py
@@ -6,10 +6,16 @@ Code related to the PSF interpolation performed by lensfit on CFHTLenS and cie.
 import os
 import subprocess
 import shutil
+import glob
+import astropy
+import numpy as np
+import math
+import copy
 
 import logging
 logger = logging.getLogger(__name__)
 
+from .. import utils as megalututils
 
 
 def writecat(cat, filepath):
@@ -26,7 +32,7 @@ def writecat(cat, filepath):
 	
 
 
-def makepsfs(cat, pointing, workdir="."):
+def makeexppsfs(cat, pointing, workdir="."):
 	"""
 	
 	Runs createPSFcube for every source in cat and on every exposure of that pointing.
@@ -86,14 +92,77 @@ def makepsfs(cat, pointing, workdir="."):
 			logger.warning(err)
 
 
-def coaddpsfs(workdir):
+def stackexppsfs(cat, workdir, gridwidth=50):
 	"""
+	I stack the PSFs from the individual exposures.
+	I do my business by looking at what I find in the workdir, autonomously.
 	
 	
+	Should this be split into stacking and making grid ?
 	"""
-	pass
+	cubestocoadd = sorted(glob.glob(os.path.join(workdir, "*.list_psfcube.fits")))
+	#print cubestocoadd
+		
+	data = astropy.io.fits.getdata(cubestocoadd[0]) # Careful, we do not do the usual transpose here !
+	assert data.shape[0] == len(cat)
+	assert data.shape[1] == data.shape[2]
 	
+	stampsize = data.shape[1]
+	
+	
+	logger.info("Will stack %i psfs from %i exposures..." % (len(cat), len(cubestocoadd)))
+	logger.info("Stamp size is %i" % (stampsize))
+
+	stack = np.zeros(data.shape) # this is for the stamps
+	counter = np.zeros(data.shape[0]) # this is to keep track of how much stamps we have for each source.
 
 
+	# We loop over the FITS files:
+	for cubetocoadd in cubestocoadd:
+		data = astropy.io.fits.getdata(cubetocoadd)
+		stack += data
+		
+		maxofstamps = np.max(np.max(data, axis=1), axis=1)
+		hasdata = maxofstamps > 0.0001 # This is boolean
+		
+		counter += hasdata.astype(int) # We add 1 where we had data
+	
+	logger.info("Done with stacking, now making grid...")
+
+	# We arrange all the stacked stamps on a grid
+	
+	gridheight = int(math.ceil(float(len(cat)) / gridwidth))
+	
+	logger.info("The grid will be %i x %i (%i tiles)" % (gridwidth, gridheight, gridwidth * gridheight))
+	
+	grid = np.zeros((gridwidth*stampsize, gridheight*stampsize))
+	
+	outcat = copy.deepcopy(cat)
+	outcat.add_columns([
+		astropy.table.Column(name="psfgridx", data=np.zeros(len(cat), dtype=float)),
+		astropy.table.Column(name="psfgridy", data=np.zeros(len(cat), dtype=float)),
+		astropy.table.Column(name="psfgridn", data=np.zeros(len(cat), dtype=int))
+	])
+	
+	for i in range(len(cat)):
+		
+		(yind, xind) = divmod(i, gridwidth)
+		xmin = xind*stampsize
+		xmax = xmin + stampsize
+		ymin = yind*stampsize
+		ymax = ymin + stampsize
+		
+		grid[xmin:xmax, ymin:ymax] = stack[i].transpose() / float(counter[i])
+		
+		xpos = xmin + stampsize/2.0
+		ypos = ymin + stampsize/2.0
+		outcat[i]["psfgridx"] = xpos
+		outcat[i]["psfgridy"] = ypos
+		outcat[i]["psfgridn"] = counter[i]
+		
+		
+	megalututils.tofits(grid, os.path.join(workdir, "psfgrid.fits"))
+	
+	return outcat
 
 
