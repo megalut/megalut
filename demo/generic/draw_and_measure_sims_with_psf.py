fa83d1fb5abed70b03d0e4395587c98b038762b9
mtewes <malte.tewes@gmail.com>
2014-09-29 14:05:22 +0200

    This is a new demo that uses the PSF stamp functionality (#8)

---
+++ demo/generic/draw_and_measure_sims_with_psf.py
@@ -0,0 +1,90 @@
+"""
+Another demo about drawing simulated galaxies.
+This time we use PSFs provided in psfs/
+"""
+
+import logging
+logging.basicConfig(level=logging.INFO)
+
+import megalut
+import megalut.sim
+import megalut.meas
+
+import numpy as np
+import astropy
+
+
+# As before, we first set the desired distributions of parameters:
+
+class MySimParams(megalut.sim.params.Params):
+	def get_flux(self):
+		return 600.0
+mysimparams = MySimParams()
+
+# And we prepare a catalog of 20 x 20 simulated galaxies:
+
+galcat = megalut.sim.stampgrid.drawcat(mysimparams, n=20, stampsize=48)
+
+print galcat[:5]
+
+
+# Now, we prepare the PSF stuff. In this case we'll use existing files:
+
+psfimg = megalut.gsutils.loadimg("psfs/psfgrid.fits")
+psfcat = megalut.utils.readpickle("psfs/cat_psfgrid.pkl")
+print psfcat[:5]
+
+# The only columns we care about are those giving the position of each PSF, called psfgridx and psfgridy in this case.
+# We will later tell drawimg to use these columns.
+# You could very well prepare a catalog containing only x and y, nothing else.
+# Note that the PSFs don't even have to be on a grid !
+
+# We also need psfcat.meta to hold the stampsize (which can be different from the galcat stampsize !)
+# For this demo catalog, it's correctly set, to 32:
+
+print psfcat.meta["stampsize"]
+
+
+# There is one more constraint:
+
+print len(psfcat)
+
+# Oops, this catalog has only 100 PSFs, but we want to simulate 400 galaxies !
+# We *have* to provide a PSF catalog of the same length as the galcat.
+# So let's build this catalog by randomly picking PSFs:
+
+matched_psfcat = psfcat[np.random.randint(low=0, high=100, size=400)] # We love astropy.table :)
+# Note that this makes a copy (not just refs), exactly as we want.
+
+
+# We are now ready to feed this into drawimg :
+
+megalut.sim.stampgrid.drawimg(galcat, psfcat=matched_psfcat, psfimg=psfimg,
+	psfxname="psfgridx", psfyname="psfgridy",
+	simgalimgfilepath="simgalimg.fits",
+	simtrugalimgfilepath="simtrugalimg.fits",
+	simpsfimgfilepath="simpsfimg.fits"
+	)
+
+# We can directly proceed by measuring the images
+
+gridimg = megalut.gsutils.loadimg("simgalimg.fits")
+meascat = megalut.meas.galsim_adamom.measure(gridimg, galcat, stampsize=48, prefix="mes")
+
+# meascat is the output catalog, it contains the measured features:
+print meascat[:5]
+
+# We save it into a pickle
+megalut.utils.writepickle(meascat, "meascat.pkl")
+
+# Let's make a simple comparision plot:
+import matplotlib.pyplot as plt
+resi_x = meascat["mes_x"] - meascat["x"]
+resi_y = meascat["mes_y"] - meascat["y"]
+flag = meascat["mes_flag"]
+plt.scatter(resi_x, resi_y, c=flag, lw=0, s=30)
+plt.xlabel("mes_x residual")
+plt.ylabel("mes_y residual")
+plt.show()
+
+
