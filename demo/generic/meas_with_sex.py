d6ee6c990d80f70007cc779f6007f6ff7fd8df32
mtewes <malte.tewes@gmail.com>
2014-10-01 10:49:11 +0200

    Adding nice warning if params are unknown. (#18)

--- demo/generic/meas_with_sex.py
+++ demo/generic/meas_with_sex.py
@@ -8,13 +8,34 @@ from megalut.meas.sextractor import SExtractor
 
 
 
-se = SExtractor()
+# Minimal example :
 
-#print se.get_version()
+"""
+se = SExtractor()
 
 # Any FITS image would be fine for this minimal example, let's use this one:
 cat = se.run("psfs/psfgrid.fits")
 
 print cat
+"""
+
+
+
+# An example with some custom configuration :
+
+
+params = ["X_IMAGE", "Y_IMAGE", "FWHM_IMAGE", "FLUX_AUTO", "BACKGROUND", "FLAGS"]
+# Note that if you give an unknown param, I will run anyway, but you will get a nice 
+# warning with a list of all common params.
+
+config = {"BACK_TYPE":"MANUAL", "BACK_VALUE":0.0}
+
+se = SExtractor(params=params, config=config)
+#print se.get_version()
+
+cat = se.run("psfs/psfgrid.fits")
+
+print cat
+
 
 
