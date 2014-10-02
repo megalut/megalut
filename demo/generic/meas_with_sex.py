5db073b9e2317d4a1f1db62b27123b507100ae66
mtewes <malte.tewes@gmail.com>
2014-10-02 16:57:33 +0200

    Almost done with ASSOC. Next is to read the output and merge the cats. (#18)

--- demo/generic/meas_with_sex.py
+++ demo/generic/meas_with_sex.py
@@ -2,13 +2,14 @@
 Standalone demo illustrating the use of the sextractor wrapper.
 """
 import logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
+import megalut
 from megalut.meas.sextractor import SExtractor
 
 
 
-# Minimal example :
+# Minimal example
 
 """
 se = SExtractor()
@@ -21,9 +22,9 @@ print cat
 
 
 
-# An example with some custom configuration :
+# An example with some custom configuration
 
-
+"""
 
 params = ["X_IMAGE", "Y_IMAGE", "FWHM_IMAGE", "BACKGROUND", "FLUX_RADIUS(3)", "FLAGS"]
 # Note that if you give an unknown param, I will run anyway, but you will get a nice 
@@ -37,4 +38,22 @@ se = SExtractor(params=params, config=config, workdir="test")
 cat = se.run("psfs/psfgrid.fits")
 
 print cat
+"""
+
+
+# An example using ASSOC
+
+# For this we need an existing catalog and a FITS image.
+
+inputcat = megalut.utils.readpickle("psfs/cat_psfgrid.pkl")
+print inputcat
+
+params = ["VECTOR_ASSOC(3)", "X_IMAGE", "Y_IMAGE", "FWHM_IMAGE", "BACKGROUND", "FLUX_RADIUS(3)", "FLAGS"]
+config = {"BACK_TYPE":"MANUAL", "BACK_VALUE":0.0, "PHOT_FLUXFRAC":"0.3, 0.5, 0.8", }
+se = SExtractor(params=params, config=config, workdir="test")
+
+cat = se.run("psfs/psfgrid.fits", assoc_cat = inputcat, assoc_xname="psfgridx", assoc_yname="psfgridy")
+
+print cat
+
 
