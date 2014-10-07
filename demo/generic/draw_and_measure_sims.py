a683ce99fc3cb12ec6760e09763b80f9e66447f2
mtewes <malte.tewes@gmail.com>
2014-10-07 11:25:23 +0200

    adapting demos by adding _ to prefix (#26)

--- demo/generic/draw_and_measure_sims.py
+++ demo/generic/draw_and_measure_sims.py
@@ -37,7 +37,7 @@ megalut.sim.stampgrid.drawimg(simcat,
 # We can directly proceed by measuring the images
 
 gridimg = megalut.gsutils.loadimg("simgalimg.fits")
-meascat = megalut.meas.galsim_adamom.measure(gridimg, simcat, stampsize=48, prefix="mes")
+meascat = megalut.meas.galsim_adamom.measure(gridimg, simcat, stampsize=48, prefix="mes_")
 
 # meascat is the output catalog, it contains the measured features:
 print meascat[:5]
