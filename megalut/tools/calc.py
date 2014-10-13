2c3aa2dfb39dac36c157695e426c35f056887cf5
mtewes <malte.tewes@gmail.com>
2014-10-13 16:15:10 +0200

    renaming rms to rmsd, as appropriate (#40_adding_tools.calc)

--- megalut/tools/calc.py
+++ megalut/tools/calc.py
@@ -4,12 +4,12 @@ A module to hold some maths or statistics oriented functions.
 
 import numpy as np
 
-def rms(tru, pre):
+def rmsd(tru, pre):
 	"""
 	Returns the RMSD between two numpy arrays
+	(only beginners tend to inexactly call this the RMS... ;-)
 	
 	http://en.wikipedia.org/wiki/Root-mean-square_deviation
-	
 	"""
 	return np.sqrt(np.mean((tru - pre)**2.0))
 
