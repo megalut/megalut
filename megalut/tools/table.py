d3bb2ded1e62c3ac3c1393c160d72575930b4a53
mtewes <malte.tewes@gmail.com>
2014-10-24 16:42:13 +0200

    Improved doc (#48)

--- megalut/tools/table.py
+++ megalut/tools/table.py
@@ -40,9 +40,9 @@ def hjoin(table1, table2, idcol):
 	
 	
 	if idcol not in table1.colnames:
-		raise RuntimeError("There is no column '%s' in table1" % (idcol))
+		raise RuntimeError("There is no ID column '%s' in table1" % (idcol))
 	if idcol not in table2.colnames:
-		raise RuntimeError("There is no column '%s' in table2" % (idcol))
+		raise RuntimeError("There is no ID column '%s' in table2" % (idcol))
 		
 	set1 = set(table1[idcol])
 	set2 = set(table2[idcol])
@@ -51,22 +51,28 @@ def hjoin(table1, table2, idcol):
 	if len(set2) != len(table2):
 		raise RuntimeError("The IDs in table2 are not unique!")
 	if set1 != set2:
-		raise RuntimeError("The IDs of your tables are not identical!")
+		raise RuntimeError("The IDs in the two tables are not identical!")
 		
-	commoncols = [col for col in table1.colnames if (col in table2.colnames)]
-	logger.info("The columns %s are common, I will keep those from table1." % (str(commoncols)))
-	# Indeed, join would duplicate and rename those columns (even if they contain the same values)
-	# Unless keys = None, in which case it would use them to identify.
-	# But then, if those cols are different, the identification fails and join would silently keep
-	# the "left" data, with masked values for the columns of table2.
-	# What we want is to raise an Error if the idcol values are fishy.
+	# Now we find out which cols to keep in table2 when calling join
+	# (without changing table2 of course).
 	
+	commoncols = [col for col in table1.colnames if col in table2.colnames]
+	if len(commoncols) > 1: # Otherwise it's just the idcol...
+		logger.info("The columns %s are common, I will keep those from table1." % (str(commoncols)))
 	table2colstokeep = [col for col in table2.colnames if col not in commoncols]
 	table2colstokeep.append(idcol)
 	
+	# Indeed, join would duplicate and rename common columns (even if they contain the same values)
+	# Unless keys = None, in which case it would use them to identify.
+	# But then, if those cols are different, the identification fails and join would silently keep
+	# the "left" data, with masked values for the columns of table2 (as if "not found")
+	# That's not what we want.
+	# What we want is to raise an Error if the idcol values are fishy.
+	
 	logger.debug("Done with security checks")
 	
-	output = astropy.table.join(table1, table2[table2colstokeep], keys=idcol, join_type='left', metadata_conflicts="error")
+	output = astropy.table.join(table1, table2[table2colstokeep], keys=idcol,
+		join_type='left', metadata_conflicts="error")
 
 	assert len(output) == len(table1)
 	assert len(output.colnames) == len(table1.colnames) + len(table2colstokeep) - 1
