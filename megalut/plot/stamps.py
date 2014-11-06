b045a5bc38690192fa241ea746dc416479d99fa4
mtewes <malte.tewes@gmail.com>
2014-11-06 15:01:31 +0100

    Improvements and doc for pngstampgrid (#60)

--- megalut/plot/stamps.py
+++ megalut/plot/stamps.py
@@ -2,21 +2,45 @@
 Functions to help visualizing postage stamps of selected sources in large images.
 """
 
+from .. import tools 
+import logging
+logger = logging.getLogger(__name__)
+
+import numpy as np
+
 try:
 	import f2n
 except ImportError:
-	logger.debug("Could not import f2n, no problem if you don't use it")
+	logger.debug("Could not import f2n, no problem if you don't use it. You can find it here: http://obswww.unige.ch/~tewes/f2n_dot_py/")
 
 
-def pngstampgrid(pngfilepath, img, catalog, xname="x", yname="y", stampsize=100, ncols=5, upsample=4, z1="auto", z2="auto"):
+def pngstampgrid(img, cat, pngfilepath, xname="x", yname="y", stampsize=100, ncols=5, upsample=4, z1="auto", z2="auto"):
 	"""
 	I write a grid of stamps corresponding to your catalog in a png image, so that you can visualize those galaxies...
-	For this I need f2n, but anyway I'm just a little helper.
+	For this I currently use the slightly outdated f2n module.
+	
+	:param img: either a galsim image or the filepath to a FITS image
+	:param cat: an astropy table
+	:param pngfilepath: path to where I should write my png file
+	:param xname: colname for the x position (in pixel)
+	:param yname: colname for y
+	:param stampsize: size in pixels of the stamps I should extract
+	:param ncols: how many columns ?
+	:param upsample: by how much should I upsample the stamps ?
+	:param z1: "z" scale low
+	:param z2: "z" scale high
+	
 	"""
-			
-	n = len(catalog)
-	nrows = int(np.ceil(float(n)/float(ncols)))
+	
+	
+	if type(img) is str:
+		logger.debug("You gave me a filepath, and I'm now loading the image...")
+		img = tools.image.loadimg(img)
 		
+	n = len(cat)
+	nrows = int(np.ceil(float(n)/float(ncols)))
+	logger.info("Preparing %i x %i stamps of %i x %i pixels each..." % (ncols, nrows, stampsize, stampsize))	
+	
 	stamprows = []
 	for nrow in range(nrows):
 		stamprow = []
@@ -24,7 +48,7 @@ def pngstampgrid(pngfilepath, img, catalog, xname="x", yname="y", stampsize=100,
 			
 			index = ncol + ncols*nrow
 			if index < n: # Then we have a galaxy to show
-				gal = catalog[index]
+				gal = cat[index]
 				(x, y) = (gal[xname], gal[yname])
 				(gps, flag) = tools.image.getstamp(x, y, img, stampsize)
 				npstamp = gps.array
@@ -38,7 +62,7 @@ def pngstampgrid(pngfilepath, img, catalog, xname="x", yname="y", stampsize=100,
 				txt = [
 					"%i (%i, %i)" % (gal.index, x, y),
 				]
-				f2nstamp.writeinfo(txt, colour=(255, 255, 100))
+				f2nstamp.writeinfo(txt, colour=255)
 				
 			else: # No more galaxies, we just fill the splot with a grey empty stamp.
 				npstamp = np.zeros((stampsize, stampsize))
