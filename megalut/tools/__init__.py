4a4a2c901bdb5efcd96f34fec1eb3cc5cdb9006e
mtewes <malte.tewes@gmail.com>
2014-11-19 15:25:39 +0100

    Implemented metrics function, including a linear regression (#72)

--- megalut/tools/__init__.py
+++ megalut/tools/__init__.py
@@ -7,4 +7,5 @@ import io
 import table
 import image
 import calc
+import metrics
 import dirs
---
+++ megalut/tools/metrics.py
@@ -0,0 +1,62 @@
+"""
+Modules for metrics computations, using astropy tables as input.
+"""
+import numpy as np
+
+import calc
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+def metrics(catalog, label, predlabel):
+	"""
+	Returns a dict with some simple standard metrics compared a label to the corresponding prediction.
+	This function explicitly takes care of masked columns, only unmasked data will be used.
+	
+	:param catalog: an astropy table containing both label and predlabel. It can be masked.
+	:param label: column name of the label
+	:param predlabel: column name of the predlabel
+	
+	:returns: a dict containing
+		
+		- **predfrac**: Fraction of predlabels / labels
+		- **rmsd**: the RMSD
+		- **m**: multiplicative bias
+		- **merr**: error on multiplicative bias
+		- **c**: additive bias
+		- **cerr**: error on additive bias
+
+	"""
+
+	logger.debug("Input catalog is masked: %s" % (catalog.masked))
+		
+	# In any case, we convert this to masked numpy arrays:
+	lab = np.ma.array(catalog[label])
+	pre = np.ma.array(catalog[predlabel])
+	
+	# We gather some info about the mask
+	ncat = len(catalog)
+	nlab = np.sum(np.logical_not(lab.mask))
+	npre = np.sum(np.logical_not(pre.mask))
+	goodmask = np.logical_or(lab.mask, pre.mask) # Indeed, to use a point, we need both values to be unmasked.
+	ngood = np.sum(np.logical_not(goodmask))
+	logger.debug("ncat = %i, nlab = %i, npre = %i, ngood = %i" % (ncat, nlab, npre, ngood))
+
+
+	# Now we manually take care of the masks here, and convert the data to simple non-masked arrays.
+	lab = np.array(lab[np.logical_not(goodmask)])
+	pre = np.array(pre[np.logical_not(goodmask)])
+	assert len(lab) == ngood and len(pre) == ngood
+	
+	# And we prepare the return dict:
+	
+	ret = {"predfrac":float(npre)/float(nlab)}
+	
+	ret["rmsd"] = calc.rmsd(lab, pre)
+	ret.update(calc.linreg(lab, pre))
+	
+	logger.debug("metrics: %s" % (ret))
+	
+	return ret
+	
