6ce35f719a35f63f46418a28275c189fd97ed741
Reiko Nakajima <reiko.nakajima@gmail.com>
2014-11-24 10:19:19 +0100

    minor bug fix; debug msg (#68)

--- megalut/meas/fdnt_measfct.py
+++ megalut/meas/fdnt_measfct.py
@@ -13,7 +13,7 @@ import megalut.meas.sewfunc_psf
 import megalut.meas.fdntfunc
 
 
-def measfct(img, catalog, psfimg=None, workdir=None, stampsize=None, psfstampsize=None):
+def measfct(img, catalog, psfimg, workdir=None, stampsize=None, psfstampsize=None):
 	"""
 	This function is custom-made for the MegaLUT GREAT3 wrapper, and it accepts those
 	keywords that great3.meas_obs() and great3.meas_sim() will pass to it.
@@ -22,13 +22,17 @@ def measfct(img, catalog, psfimg=None, workdir=None, stampsize=None, psfstampsiz
 	
 	# Run SExtractor on gals
 	sexpath = "sex"
-	outcat = megalut.meas.sewfunc.measure(img, catalog, workdir=workdir, sexpath=sexpath)
+	outcat = megalut.meas.sewfunc.measure(img, catalog, workdir=workdir, sexpath=sexpath, prefix='')
 
 	# Run SExtractor on PSFs
+	print
+	print "IT BEGINS HERE"
+	print
+
 	outcat = megalut.meas.sewfunc_psf.measure(img, outcat, workdir=workdir, sexpath=sexpath)
 
 	# Run FDNT:
-	outcat = megalut.meas.fdntfunc.measure(img, catalog, psfimg, 
+	outcat = megalut.meas.fdntfunc.measure(img, outcat, psfimg,
 		psfxname="psfx", psfyname="psfy", stampsize=stampsize, psfstampsize=psfstampsize)
 	
 	return outcat
