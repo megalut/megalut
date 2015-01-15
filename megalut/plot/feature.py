5a44ce8c664864a3b2f1391dc92aef72ba83ae38
mtewes <malte.tewes@gmail.com>
2015-01-15 15:40:04 +0100

    Adding errcolname (#59)

--- megalut/plot/feature.py
+++ megalut/plot/feature.py
@@ -3,18 +3,22 @@ class Feature():
 	"""
 	This class holds a column name together with an interval to nicely show 
 	this column values on plots.
-	It is not tied to a particular table.
+	One can also specify a column name with uncertainties (that will typically be shown as 1-sigma error bars).
+	It is not tied to a particular table, nor to a particular plot: the same "Feature" object can be reused for different tables and plots.
 	"""
-	def __init__(self, colname, low=None, high=None, nicename=None):
+	def __init__(self, colname, low=None, high=None, nicename=None, errcolname=None):
 		"""
 
 		:param colanme: name of a column 
 		:param low: lower bound of the interval to show on the plot.
 		:param high: higher bound
 		:param nicename: A nicer string designating the feature, to be used as axis label.
+		:param errcolname: name of a column  
+		
 		"""
 		
 		self.colname = colname
+		self.errcolname = errcolname
 		self.low = low
 		self.high = high
 		
