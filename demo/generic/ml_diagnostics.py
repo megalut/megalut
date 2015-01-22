ca3dd891125213d7ac48648d84db77fdfd966fac
kuntzer <thibault.kuntzer@epfl.ch>
2015-01-22 14:38:38 +0100

    better docstrings (#86)

--- demo/generic/ml_diagnostics.py
+++ demo/generic/ml_diagnostics.py
@@ -9,7 +9,6 @@ logging.basicConfig(level=logging.INFO)
 
 import megalut
 import matplotlib.pyplot as plt
-import megalut.plot
 import megalut.diagnostics
 
 inputcat = megalut.tools.io.readpickle("meascat.pkl")
@@ -22,31 +21,4 @@ diagnostics.compare_distrib(fig1)
 
 fig2 = plt.figure()
 diagnostics.compare_distrib(fig2, error=True)
-plt.show()
-
-exit()
-
-traincat = myml.predict(inputcat)
-validationcat = traincat[myml.training_set_index:]
-traincat = traincat[:myml.training_set_index]
-
-tru_g1 = megalut.plot.feature.Feature("tru_g1", -0.6, 0.6, "Nice label for true g1")
-pre_g1 = megalut.plot.feature.Feature("pre_g1") # Minimal call, just to highlight the default behavior.
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-
-res = megalut.plot.qqplot.qqplot(ax, traincat, pre_g1, label="Train", color="b", edgecolor="None")
-res = megalut.plot.qqplot.qqplot(ax, validationcat, pre_g1, label="Cross-validation", color="g", edgecolor="None")
-plt.grid()
-plt.legend(loc=2)
-
-plt.xlabel("Normal distribution quantiles")
-plt.ylabel("Data quantiles")
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-
-megalut.plot.qqplot.qqplot2dataset(ax, traincat, validationcat, pre_g1)
-
 plt.show()
--- megalut/diagnostics/learn.py
+++ megalut/diagnostics/learn.py
@@ -4,9 +4,16 @@ from .. import plot
 from .. import tools
 from astropy.table import Table
 
-class learn():
+class Learn():
+	"""
+	The class containing the diagnostics for testing the robustness of the learning
+	"""
 	
 	def __init__(self, myml, inputcat):
+		"""
+		:param myml: a ML instance
+		:param inputcat: the input astropy table used for training
+		"""
 		self.myml = myml
 		
 		self.inputcat = inputcat
@@ -20,19 +27,47 @@ class learn():
 
 	def compare_distrib(self, fig, prelabels=None, error=False, trulabels=None, fit_linreg=True,
 					fitlinekwargs=None, titles=None, show_id_line=True, idlinekwargs=None, **kwargs):
+		"""
+		Make a QQ-plot of the predictions after the training of the training and cross-validation set
 		
+		:param fig: a figure instance of pyplot
+		:param prelabels: a list containing all the columns to show. If set to None, will find automatically
+			the columns to plot
+		:param error: if set to `True`, will show the RMSD from the truth, else simply the distribution of the prediction
+		:param trulabels: the corresponding columns for `prelabels` if `error` is `True`, same None behaviour.
+		:param fit_linreg: Fits and shows a linear fit through the QQ-plot.
+		:param fitlinekwargs: kwargs for `fit_linreg`
+		:param titles: must be of the same lenght as `prelabels`, contains the titles of the subplots
+		:param show_id_line: shows the identity line
+		:param idlinekwargs: kwargs for show_id_line
+		
+	
+		All further **kwargs** are passed to axes.plot()
+		"""
+		
+		# Get all the colnames that start by "pre" and construct the prelabels if needed
 		if prelabels is None:
 			prelabels = [item for item in self.traincat.colnames if item.startswith('pre')]
 			if error:
 				trulabels = ["tru" + i[3:] for i in prelabels]
 		
 		total = len(prelabels)
-		assert total < 10
 		
+		# Make a few security checks
+		assert total < 10
+		if not len(prelabels) == len(trulabels):
+			raise RuntimeError("Your lists prelabels and trulabels do not have the same size")
+		
+		if (titles is not None) and (not len(prelabels) == len(trulabels)):
+			raise RuntimeError("Your lists prelabels and trulabels do not have the same size")
+		
+		# Start plotting
 		for i in range(len(prelabels)):
+			# Shorthand notation
 			datax = self.traincat
 			datay = self.validationcat
 			
+			# If error, then compute the RMSD and build a new astropy array
 			if error:
 				newlabel = "RMSD %s" % prelabels[i]
 				
@@ -46,12 +81,15 @@ class learn():
 				
 				prelabels[i] = newlabel
 			
+			# Construct the feature object to select the right column in the astopy table
 			feat = plot.feature.Feature(prelabels[i])
 			
+			# Handles the subplot number
 			plot_id = 100 + total * 10 + i + 1
 			
 			ax = fig.add_subplot(plot_id)
 		
+			# Actually draws the QQ plot
 			xx, yy = plot.qqplot.qqplot2dataset(ax, datax, datay, feat, **kwargs)
 			
 			if show_id_line: # Show the "diagonal" identity line
@@ -73,7 +111,7 @@ class learn():
 					ax.axvline(0, **myidlinekwargs)
 					ax.axhline(0, **myidlinekwargs)
 						
-			if fit_linreg:
+			if fit_linreg: # Show the fit line
 				ret = tools.calc.linreg(xx,yy)
 				
 				# And we plot the line:
@@ -86,7 +124,10 @@ class learn():
 
 				ax.plot(dx, dx * (ret['m'] + 1) + ret['c'], **myfitlinekwargs)
 				
-				ax.annotate("Slope = %1.3f\nIntercept = %1.3f" % (ret['m'] + 1, ret['c']), 
+				# Show the slope and intercept value
+				text = r"Slope $%1.3f\pm%1.3f$" % (ret['m'] + 1, ret['merr']) + "\n"
+				text += r"Intercept $%1.3f\pm%1.3f$" % (ret['c'], ret['cerr'])
+				ax.annotate(text, 
 						xy=(0.0, 1.0), xycoords='axes fraction', xytext=(8, -8), 
 						textcoords='offset points', ha='left', va='top')
 			
