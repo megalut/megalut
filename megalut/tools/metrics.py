ddcf8686641417e6d7e21964ce253b126b23c6c5
mtewes <malte.tewes@gmail.com>
2015-01-27 11:52:32 +0100

    Cleaning up metrics() so that it uses cutmasked() (#93)

--- megalut/tools/metrics.py
+++ megalut/tools/metrics.py
@@ -4,6 +4,7 @@ Modules for metrics computations, using astropy tables as input.
 import numpy as np
 
 import calc
+import table
 
 import logging
 logger = logging.getLogger(__name__)
@@ -20,9 +21,7 @@ def metrics(catalog, label, predlabel):
 	
 	:returns: a dict containing
 		
-		- **labfrac**: Fraction of rows of the catalog that have the label (quite boring, usually exactly 1.0)
-		- **predfrac**: Fraction of predlabels / labels.
-			For example, 0.5 means that only half of the rows having a label value also have a predlabel value.
+		- **predfrac**: Fraction of rows having an unmasked predlabel
 		- **rmsd**: the RMSD
 		- **m**: multiplicative bias (slope minus one)
 		- **merr**: error on multiplicative bias
@@ -32,34 +31,17 @@ def metrics(catalog, label, predlabel):
 	"""
 	
 	logger.debug("Computing metrics for label = %s and predlabel = %s" % (label, predlabel))
-	logger.debug("Input catalog is masked: %s" % (catalog.masked))
-		
-	# In any case, we convert this to masked numpy arrays:
-	lab = np.ma.array(catalog[label])
-	pre = np.ma.array(catalog[predlabel])
 	
-	# We gather some info about the mask
-	ncat = len(catalog)
-	nlab = np.sum(np.logical_not(lab.mask))
-	npre = np.sum(np.logical_not(pre.mask))
-	goodmask = np.logical_or(lab.mask, pre.mask) # Indeed, to use a point, we need both values to be unmasked.
-	ngood = np.sum(np.logical_not(goodmask))
-	logger.debug("ncat = %i, nlab = %i, npre = %i, ngood = %i" % (ncat, nlab, npre, ngood))
-
-
-	# Now we manually take care of the masks here, and convert the data to simple non-masked arrays.
-	lab = np.array(lab[np.logical_not(goodmask)])
-	pre = np.array(pre[np.logical_not(goodmask)])
-	assert len(lab) == ngood and len(pre) == ngood
+	# We get the unmasked points :
+	metcat = table.cutmasked(catalog, colnames=[label, predlabel])	
+	lab = metcat[label]
+	pre = metcat[predlabel]
 	
 	# And we prepare the return dict:
-	
-	ret = {"labfrac":float(nlab)/float(ncat), "predfrac":float(npre)/float(nlab)}
+	ret = {"predfrac":float(len(metcat))/float(len(catalog))}
 	
 	ret["rmsd"] = calc.rmsd(lab, pre)
 	ret.update(calc.linreg(lab, pre))
-	
-	#logger.debug("metrics: %s" % (ret))
 	
 	return ret
 	
