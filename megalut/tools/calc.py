257fc0ceab6c572b6cfca6cb6ab422a538ccaf74
mtewes <malte.tewes@gmail.com>
2015-08-05 18:24:52 +0200

    adding rotg function

--- megalut/tools/calc.py
+++ megalut/tools/calc.py
@@ -8,6 +8,19 @@ import scipy.stats
 import logging
 logger = logging.getLogger(__name__)
 
+
+
+def rotg(g1, g2, deg):
+	"""
+	Returns the rotated (g1, g2) by deg degrees.
+	Be careful, there is this "extra" factor 2 here, given the meaning of g1 and g2.
+	"""
+	
+	complexg = g1 + g2*1j
+	rotg = complexg * np.exp(1j * 2.0 * np.pi*deg/180.0)
+	return (rotg.real, rotg.imag)
+
+
 def rmsd(x, y):
 	"""
 	Returns the RMSD between two numpy arrays
