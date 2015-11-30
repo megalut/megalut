72997edfcd1f35a909f4e7e08353c855d5aae84f
mtewes <malte.tewes@gmail.com>
2015-11-30 15:41:01 +0100

    First upload of SDC scripts

---
+++ runs/malte/sdcsbe/megalutconfig/measfct.py
@@ -0,0 +1,38 @@
+import megalut.meas
+
+def default(catalog):
+	"""
+	Default measfct, runs on "img".
+	"""	
+	
+	# HSM adamom
+	catalog = megalut.meas.galsim_adamom.measfct(catalog, stampsize=150, variant="wider")
+	
+	# And skystats
+	catalog = megalut.meas.skystats.measfct(catalog, stampsize=150)
+	
+	# And snr
+	catalog = megalut.meas.snr.measfct(catalog, gain=3.5)
+	
+	return catalog
+
+
+default_groupcols = [
+'adamom_flag',
+'adamom_flux',
+'adamom_x',
+'adamom_y',
+'adamom_g1',
+'adamom_g2',
+'adamom_sigma',
+'adamom_rho4',
+'skystd',
+'skymad',
+'skymean',
+'skymed',
+'skystampsum',
+'skyflag',
+'snr',
+]
+
+default_removecols = []
