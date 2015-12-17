0503b451014ade7cf98423340af01e2a547edd49
mtewes <malte.tewes@gmail.com>
2015-12-17 10:29:23 +0100

    moving bining to utils and first versin of color-bin

--- megalut/plot/bin.py
+++ megalut/plot/bin.py
@@ -24,16 +24,236 @@ logger = logging.getLogger(__name__)
 
 
 
-def res(ax, cat, featx, featy, nbins=10, selector=None, showselector=True, title=None, showidline=True, metrics=False,
+
+
+def res(ax, cat, feattru, featpre, featc=None, nbins=10, ncbins=3, ebarmode="bias", showidline=True, metrics=True):
+	"""
+	Shows residues of a predicted feature featpre in bins of the corresponding truth feattru.
+	If featc is specified (c stands for color), this analysis is done in several bins of featc
+	(say to check that the shear estimate is unbiased for all galaxy sizes...).
+	
+	Trick: the low and high of featpre are used to define the plot range for the residues.
+	
+	"""
+
+	logger.info("Preparing res plot of '{}' in bins of '{}'".format(featpre.colname, feattru.colname))
+	
+
+	# First, we get the 1D data, removing all masked points.
+	features = [feattru, featpre]
+	if featc is not None:
+		features.append(featc)
+	data = tools.feature.get1Ddata(cat, features, keepmasked=False)
+	
+	# We compute the residues, for every point.
+	assert "res" not in data.colnames
+	data["res"] = data[featpre.colname] - data[feattru.colname]
+	
+	# Defining the bins of the x axis:
+	xbinrange = utils.getrange(data, feattru)
+	
+	# And for the "color" axis:
+	if featc is not None:
+		logger.info("Building bins in {}...".format(featc.colname))
+		cbinrange = utils.getrange(data, featc)
+		
+		cbinlims = np.linspace(cbinrange[0], cbinrange[1], ncbins+1)
+		
+		cbinlows = cbinlims[0:-1]
+		cbinhighs = cbinlims[1:]
+		cbincenters = 0.5 * (cbinlows + cbinhighs)
+		assert len(cbincenters) == ncbins
+		
+
+		coloriter=iter(matplotlib.cm.brg(np.linspace(0,1,ncbins)))
+		offsetscale = 0.3*((xbinrange[1] - xbinrange[0])/float(nbins))/float(ncbins)
+
+		for i in range(ncbins):
+			
+			color = next(coloriter)
+			label = "{0:.0f} < {1} < {2:.0f}".format(cbinlows[i], featc.nicename, cbinhighs[i])
+			offset = (i - float(ncbins)/2) * offsetscale
+			
+			# We build the subset of data that is in this color bin:
+			selcbin = tools.table.Selector(label, [("in", featc.colname, cbinlows[i], cbinhighs[i])])
+			cbindata = selcbin.select(data)
+			if len(cbindata) == 0:
+				continue
+			
+			# And now bin this in x:
+			bindata = utils.summabin(cbindata[feattru.colname], cbindata["res"], xbinrange=xbinrange, nbins=nbins)
+			
+			errorbarkwargs = {"color":color, "ls":"-", "marker":".", "lw":1.0, "mew":1.0}
+	
+			#yerr = bindata["ystds"]
+			yerr = np.array([bindata["ylowps"], bindata["yhighps"]])
+			yerrbias = yerr / np.sqrt(bindata["ns"])
+	
+			if ebarmode == "scatter":
+				ax.errorbar(bindata["xbincents"]+offset, bindata["ymeans"], yerr=yerr, label=label, **errorbarkwargs)
+			elif ebarmode == "bias":
+				ax.errorbar(bindata["xbincents"]+offset, bindata["ymeans"], yerr=yerrbias, label=label, **errorbarkwargs)
+	
+
+
+	else:	
+	
+		bindata = utils.summabin(data[feattru.colname], data["res"], xbinrange=xbinrange, nbins=nbins)
+	
+		errorbarkwargs = {"color":"black", "ls":"None", "marker":".", "lw":1.0, "mew":1.0}
+	
+		#yerr = bindata["ystds"]
+		yerr = np.array([bindata["ylowps"], bindata["yhighps"]])
+		yerrbias = yerr / np.sqrt(bindata["ns"])
+	
+		if ebarmode == "scatter":
+			ax.errorbar(bindata["xbincents"], bindata["ymeans"], yerr=yerr, **errorbarkwargs)
+		elif ebarmode == "bias":
+			ax.errorbar(bindata["xbincents"], bindata["ymeans"], yerr=yerrbias, **errorbarkwargs)
+	
+	
+		
+	if showidline: # Show the identity line
+		idlinekwargs = {"ls":"--", "color":"gray", "lw":1}
+		ax.plot(xbinrange, (0.0, 0.0), **idlinekwargs)
+
+	# We want minor ticks:
+	ax.xaxis.set_minor_locator(AutoMinorLocator(5))
+	ax.yaxis.set_minor_locator(AutoMinorLocator(5))
+	
+	ax.set_ylabel(featpre.nicename + " - " + feattru.nicename)
+	ax.set_ylim(featpre.low, featpre.high)
+	ax.set_xlabel(feattru.nicename)
+	ax.set_xlim(xbinrange)
+
+	ax.legend()
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+"""	
+	binlims = np.linspace(binrange[0], binrange[1], nbins+1)
+	bincenters = 0.5 * (binlims[0:-1] + binlims[1:])
+	assert len(bincenters) == nbins
+	
+	binindices = np.digitize(data[feattru.colname], bins=binlims)
+	inrangeindices = np.arange(nbins)+1 # those indices that are within the binlims
+	assert len(inrangeindices) == nbins
+	
+	
+	# Defining the bins in "colour"
+	if featc is not None:
+		cbinrange = utils.getrange(data, featc)
+		cbinlims = np.linspace(cbinrange[0], cbinrange[1], ncbins+1)
+		cbincenters = 0.5 * (cbinlims[0:-1] + cbinlims[1:])
+		assert len(cbincenters) == ncbins
+		
+	
+	ymeans = []
+	ystds = []
+	ylowps = []
+	yhighps = []
+	ns = []
+		
+	for ind in inrangeindices: # We loop over the bins
+		
+		inbools = binindices == ind # a boolean array
+		nin = np.sum(inbools)
+		
+		if nin < 2:
+			ymeans.append(np.nan)
+			ystds.append(np.nan)
+			ylowps.append(np.nan)
+			yhighps.append(np.nan)
+			ns.append(np.nan)
+			continue
+		
+		ns.append(nin)
+		
+		thesexvals = data[feattru.colname][inbools]
+		theseyvals = data["res"][inbools]
+		
+		#assert len(thesexvals) == nin
+		#assert len(theseyvals) == nin
+		
+		ymeans.append(np.mean(theseyvals))
+		ystds.append(np.std(theseyvals))
+		
+		ylowps.append(np.fabs(np.percentile(theseyvals, 15.8) - np.mean(theseyvals)))
+		yhighps.append(np.fabs(np.percentile(theseyvals, 84.1) - np.mean(theseyvals)))
+		
+		
+				
+	#errorbarkwargs = {"capthick":0, "zorder":-100}
+	errorbarkwargs = {"color":"black", "ls":"None", "marker":".", "lw":1.0, "mew":1.0}
+	
+	#yerr = ystds
+	yerr = np.array([ylowps, yhighps])
+	yerr_bias = yerr / np.sqrt(np.array(ns))
+	yerr_stdbias = np.array(ystds) / np.sqrt(np.array(ns))
+	
+	if ebarmode == "scatter":
+		ax.errorbar(bincenters, ymeans, yerr=yerr, **errorbarkwargs)
+	elif ebarmode == "bias":
+		ax.errorbar(bincenters, ymeans, yerr=yerr_bias, **errorbarkwargs)
+	
+	#yerr = np.array(ystds) / np.sqrt(np.array(ns))
+	#ax.errorbar(bincenters, ymeans, yerr=yerr, color="red", ls="None", marker=".")
+	
+	if showidline: # Show the identity line
+		idlinekwargs = {"ls":"--", "color":"gray", "lw":1}
+		ax.plot(binrange, (0.0, 0.0), **idlinekwargs)
+
+
+	# We want minor ticks:
+	ax.xaxis.set_minor_locator(AutoMinorLocator(5))
+	ax.yaxis.set_minor_locator(AutoMinorLocator(5))
+	
+	ax.set_ylabel(featpre.nicename + " - " + feattru.nicename)
+	ax.set_ylim(featpre.low, featpre.high)
+	ax.set_xlabel(feattru.nicename)
+	ax.set_xlim(binrange)
+	
+	
+"""	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def bin(ax, cat, featx, featy, nbins=10, selector=None, showselector=True, title=None, showidline=True, metrics=False,
 	ebarmode="scatter"):
 	"""
-	Shows the residues featy in bins of featx.
+	Summarizes featy in bins of featx.
 	
 	:param ebarmode: controls what should be shown as error-bars
 	
 	"""
 
-	logger.info("Preparing res plot of '{}' versus '{}'".format(featy.colname, featx.colname))
+	logger.info("Preparing bin plot of '{}' versus '{}'".format(featy.colname, featx.colname))
 	
 	nall = len(cat)
 	nsel = nall
--- megalut/plot/utils.py
+++ megalut/plot/utils.py
@@ -3,6 +3,10 @@ Helper functions for plots
 Stuff that can be useful in many plots, to avoid repeading it...
 """
 
+import numpy as np
+import copy
+
+
 
 def showselector(ax, sel):
 	"""
@@ -17,40 +21,107 @@ def showselector(ax, sel):
 
 
 
+def getrange(cat, feature):
+	"""
+	Returns a range (hi, low) to use when plotting the feature, respecting the feature's settings if not "None".
+	So it makes sure you get actual numbers.
+	"""
+
+
+	if feature.low is not None and feature.high is not None: 
+		customrange = (feature.low, feature.high)
+	else:
+		customrange = (np.min(cat[feature.colname]), np.max(cat[feature.colname]))
+
+	return customrange
 
 
 
 
+def summabin(x, y, xbinrange=(None, None), nbins=10):
+	"""
+	For two 1D numpy arrays x and y, summarizes the y data in bins of x.
+	
+	Returns a dict with arrays containing different information for each bin:
+	
+	xbincents, xbinlows, xbinhighs,
+	ymeans, ystds, ylowps, yhighps,
+	ns
+	
+	
+	"""
+	
+	# Some tests:
+	assert x.ndim == 1
+	assert y.ndim == 1
+	assert x.size == y.size
+	assert x.size > 1
+	
+	# Defining the bin range of the x axis:
+	br = list(copy.deepcopy(xbinrange))
+	if br[0] is None:
+		br[0] = np.min(x)
+	if br[1] is None:
+		br[1] = np.max(x)
+	
+	# And now defining the bins:
+	binlims = np.linspace(br[0], br[1], nbins+1)
+	bincenters = 0.5 * (binlims[0:-1] + binlims[1:])
+	assert len(bincenters) == nbins
+	
+	binindices = np.digitize(x, bins=binlims)
+	inrangeindices = np.arange(nbins)+1 # those indices that are within the binlims
+	assert len(inrangeindices) == nbins
+	
 
-#import numpy as np
-#from .. import tools
-#
-#import logging
-#logger = logging.getLogger(__name__)
-#
-#
-#def getdata(cat, features):
-#	"""
-#	Prepares a copy of cat containing only those rows for which none of the features is masked.
-#	
-#	:param cat: an astropy table
-#	:param features: a list of Feature objects
-#	
-#	:returns: a table containing only the available rows and required columns. 
-#	
-#	"""
-#	# Potentially, these columns can exist:	
-#	colnames = []
-#	for feature in features:
-#		colnames.append(feature.colname)
-#		if feature.errcolname != None:
-#			colnames.append(feature.errcolname)
-#	
-#	colnames = list(set(colnames)) # To ensure that they are unique
-#	
-#	# We get rid of rows with masked content:
-#	nomaskcat = tools.table.cutmasked(cat, colnames, keep_all_columns=False)
-#	# Indeed for plots we don't need the other columns !
-#	# It's safer to remove them.
-#	
-#	return nomaskcat
+	# And now we loop over the bins to compute our stuff.
+	
+	ymeans = []
+	ystds = []
+	ylowps = []
+	yhighps = []
+	ns = []
+		
+	for ind in inrangeindices: # We loop over the bins
+		
+		inbools = binindices == ind # a boolean array
+		nin = np.sum(inbools) # number of values falling into the current bin
+		
+		if nin < 2:
+			ymeans.append(np.nan)
+			ystds.append(np.nan)
+			ylowps.append(np.nan)
+			yhighps.append(np.nan)
+			ns.append(np.nan)
+			continue
+		
+		ns.append(nin)
+		
+		thesexvals = x[inbools]
+		theseyvals = y[inbools]
+		thisymean = np.mean(theseyvals)
+				
+		ymeans.append(thisymean)
+		ystds.append(np.std(theseyvals))
+		
+		ylowps.append(np.fabs(np.percentile(theseyvals, 15.8) - thisymean))
+		yhighps.append(np.fabs(np.percentile(theseyvals, 84.1) - thisymean))
+		
+	
+	ret = {
+		"xbincents":np.array(bincenters),
+		"xbinlows":np.array(binlims[0:-1]),
+		"xbinhighs":np.array(binlims[1:]),
+		"ymeans":np.array(ymeans), 
+		"ystds":np.array(ystds), 
+		"ylowps":np.array(ylowps), 
+		"yhighps":np.array(yhighps),
+		"ns":np.array(ns)
+	}	
+	
+	for key in ret.keys():
+		assert len(ret[key]) == nbins
+	
+	return ret
+
+
