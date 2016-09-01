ed94fe8b73823cf42aa470c16c5445cc9f64fa93
mtewes <malte.tewes@gmail.com>
2016-09-01 15:44:30 +0200

    Cleanup of the megalutgreat3 wrapper GREAT3Run object

--- megalut/tools/dirs.py
+++ megalut/tools/dirs.py
@@ -5,9 +5,12 @@ logger = logging.getLogger(__name__)
 
 
 def mkdir(somedir):
-    """
-    A wrapper around os.makedirs, with some logging.
-    :param somedir: a name or path to a directory which I should make.
-    """
-    if not os.path.isdir(somedir):
-        os.makedirs(somedir)
+	"""
+	A wrapper around os.makedirs, with some logging.
+	:param somedir: a name or path to a directory which I should make.
+	"""
+	
+	raise RuntimeError("I suggest to stop using this, it doesn't even log so I don't see the point.")
+	
+	if not os.path.isdir(somedir):
+		os.makedirs(somedir)
