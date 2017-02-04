847cfb991e3d2b21618c37d36ef65cc236fdcd0d
kuntzer <thibault.kuntzer@epfl.ch>
2017-02-04 18:54:12 +0100

    adding run sim val

---
+++ runs/fiducial/ellipticity-varpsf/run_sim_validation.py
@@ -0,0 +1,70 @@
+import os
+
+import megalut.sim
+import megalut.meas
+import megalut.tools as tls
+import measfcts
+import simparams
+import psf
+
+import includes
+
+import logging
+logger = logging.getLogger(__name__)
+
+simdir = includes.simvaldir
+
+# Let's train for ellipticity
+# We do not need Shape Noise Cancellation and no shear needeed
+sp = simparams.EllipticityVarPSF()
+# Let's use the same PSF field as for the training
+psf_field = tls.io.readpickle(os.path.join(includes.simdir, sp.name, "psf", "psf_field.pkl"))
+sp.set_psf_field(psf_field)
+sp.shear = 0.1
+sp.snc_type = 10000
+sp.noise_level = 0.8
+n = 1
+nc = 1
+ncat = 2500
+nrea = 1
+
+megalut.sim.run.multi(
+	simdir=simdir,
+	simparams=sp,
+	drawcatkwargs={"n":n, "nc":nc, "stampsize":includes.stampsize},
+	drawimgkwargs={}, 
+	psfcat=None, psfselect="random",
+	ncat=ncat, nrea=nrea, ncpu=includes.ncpu,
+	savepsfimg=True, savetrugalimg=False
+	)
+
+
+
+megalut.meas.run.onsims(
+	simdir=simdir,
+	simparams=sp,
+	measdir=simdir,
+	measfct=measfcts.default,
+	measfctkwargs={"stampsize":includes.stampsize},
+	ncpu=includes.ncpu,
+	skipdone=True
+	)
+
+cat = megalut.meas.avg.onsims(
+	measdir=simdir, 
+	simparams=sp,
+	task="group",
+	groupcols=measfcts.default_groupcols, 
+	removecols=measfcts.default_removecols
+	)
+
+
+
+megalut.tools.table.keepunique(cat)
+print megalut.tools.table.info(cat)
+megalut.tools.io.writepickle(cat, os.path.join(simdir, sp.name, "groupmeascat.pkl"))
+
+cat = megalut.tools.table.groupreshape(cat, groupcolnames=["tru_g1", "tru_g2", "tru_g", "tru_flux", "tru_rad"])
+megalut.tools.table.keepunique(cat)
+print megalut.tools.table.info(cat)
+megalut.tools.io.writepickle(cat, os.path.join(simdir, sp.name, "groupmeascat_cases.pkl"))
