1dd69b8f58604db6fe4f4660003bc9408917d2a6
kuntzer <thibault.kuntzer@epfl.ch>
2017-03-02 13:32:00 +0100

    working g3-like

--- runs/fiducial/g3like/run_sim_validation.py
+++ runs/fiducial/g3like/run_sim_validation.py
@@ -3,7 +3,7 @@ import os
 import megalut.sim
 import megalut.meas
 import measfcts
-import simparams3D as simparams
+import simparams#3D as simparams
 
 import includes
 
@@ -14,17 +14,15 @@ logger = logging.getLogger(__name__)
 
 # Let's simulate a validation dataset for ellipticity only
 # We do not need Shape Noise Cancellation and no shear needeed
-sp = simparams.Ellipticity3D()
+sp = simparams.Sersics_statshear()
 sp.shear = 0.1
 simdir = includes.simvaldir
 sp.snc_type = 4
 sp.noise_level = 0.8
-n = 100
+n = 250
 nc = 1
-ncat = 250
+ncat = 300
 nrea = 1
-
-"""
 
 megalut.sim.run.multi(
 	simdir=simdir,
@@ -47,7 +45,7 @@ megalut.meas.run.onsims(
 	ncpu=includes.ncpu,
 	skipdone=True
 	)
-"""
+
 cat = megalut.meas.avg.onsims(
 	measdir=simdir, 
 	simparams=sp,
@@ -62,7 +60,7 @@ megalut.tools.table.keepunique(cat)
 print megalut.tools.table.info(cat)
 megalut.tools.io.writepickle(cat, os.path.join(simdir, sp.name, "groupmeascat.pkl"))
 
-cat = megalut.tools.table.groupreshape(cat, groupcolnames=["tru_g1", "tru_g2", "tru_g", "tru_flux", "tru_rad"])#"tru_s1", "tru_s2"])
+cat = megalut.tools.table.groupreshape(cat, groupcolnames=["tru_s1", "tru_s2"])#["tru_g1", "tru_g2", "tru_g", "tru_flux", "tru_rad"])#"tru_s1", "tru_s2"])
 megalut.tools.table.keepunique(cat)
 print megalut.tools.table.info(cat)
 megalut.tools.io.writepickle(cat, os.path.join(simdir, sp.name, "groupmeascat_cases.pkl"))
