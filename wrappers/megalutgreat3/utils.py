7d354158a58603fbe751dae89117ff287d18e248
mtewes <malte.tewes@gmail.com>
2017-03-28 10:47:39 +0200

    Metrics evaluation code

--- wrappers/megalutgreat3/utils.py
+++ wrappers/megalutgreat3/utils.py
@@ -143,4 +143,102 @@ class Branch:
 			folder = self.branchdir(truth=True)
 		return os.path.join(folder, 'shear_params-%03i.txt' % (subfield))
 	
+	def trustarfilepath(self, subfield, folder=None):
+		"""
+		The psf g1 g2, used to "rotate" metrics
+		"""
+		if folder is None:
+			folder = self.branchdir(truth=True)
+		return os.path.join(folder, 'starshape_parameters-%03i-0.txt' % (subfield))
 	
+
+
+import megalut.tools.calc
+import numpy as np
+
+def metrics(cat, trucols, predcols, psfgcols, latex=True):
+	"""
+	Computes GREAT3 metrics m and c for 1, 2, + and x, including errors.
+	trucols and predcols are tuples with column names, e.g. ("tru_s1", "tru_s2")
+	
+	It has been checked that results are identical to what the public great3 code gives (on the "simple" branches only).
+	"""
+	
+	returndict = {}
+	
+	assert cat[trucols[0]].ndim == 1
+	assert cat[predcols[1]].ndim == 1
+
+	
+	pixres1 = megalut.tools.calc.linreg(cat[trucols[0]], cat[predcols[0]], prob=0.68) # Component 1
+	pixres2 = megalut.tools.calc.linreg(cat[trucols[1]], cat[predcols[1]], prob=0.68) # Component 2
+	
+	returndict["m1"] = pixres1["m"]
+	returndict["m1err"] = pixres1["merr"]
+	returndict["c1"] = pixres1["c"]
+	returndict["c1err"] = pixres1["cerr"]
+	
+	returndict["m2"] = pixres2["m"]
+	returndict["m2err"] = pixres2["merr"]
+	returndict["c2"] = pixres2["c"]
+	returndict["c2err"] = pixres2["cerr"]
+	
+	
+	# Now the rotated metrics
+	# This simply follows great3-public/metrics/evaluate
+		
+	# angles:
+	rotations = .5 * np.arctan2(cat[psfgcols[1]], cat[psfgcols[0]]) # Note the inverted order of components
+		
+	# rotating predictions:
+	g1prot = cat[predcols[0]] * np.cos(-2. * rotations) - cat[predcols[1]] * np.sin(-2. * rotations)
+	g2prot = cat[predcols[0]] * np.sin(-2. * rotations) + cat[predcols[1]] * np.cos(-2. * rotations)
+	
+	# rotating truth:
+	g1trot = cat[trucols[0]] * np.cos(-2. * rotations) - cat[trucols[1]] * np.sin(-2. * rotations)
+	g2trot = cat[trucols[0]] * np.sin(-2. * rotations) + cat[trucols[1]] * np.cos(-2. * rotations)
+ 	
+	
+	pixresplus = megalut.tools.calc.linreg(g1trot, g1prot, prob=0.68)
+	pixrescros = megalut.tools.calc.linreg(g2trot, g2prot, prob=0.68)
+	
+	returndict["m+"] = pixresplus["m"]
+	returndict["m+err"] = pixresplus["merr"]
+	returndict["c+"] = pixresplus["c"]
+	returndict["c+err"] = pixresplus["cerr"]
+
+	returndict["mx"] = pixrescros["m"]
+	returndict["mxerr"] = pixrescros["merr"]
+	returndict["cx"] = pixrescros["c"]
+	returndict["cxerr"] = pixrescros["cerr"]
+
+
+
+	if latex is True:
+		
+		kilo = {}
+		for (key, value) in returndict.iteritems():
+			kilo[key] = 1000.0 * value
+		
+		ret = ["Branch & ",
+			"${r[m1]:+.2f}\pm{r[m1err]:.2f}$ & ".format(r=kilo),
+			"${r[m2]:+.2f}\pm{r[m2err]:.2f}$ & ".format(r=kilo),
+			"${r[m+]:+.2f}\pm{r[m+err]:.2f}$ & ".format(r=kilo),
+			"${r[mx]:+.2f}\pm{r[mxerr]:.2f}$ & ".format(r=kilo),
+			"${r[c1]:+.2f}\pm{r[c1err]:.2f}$ & ".format(r=kilo),
+			"${r[c2]:+.2f}\pm{r[c2err]:.2f}$ & ".format(r=kilo),
+			"${r[c+]:+.2f}\pm{r[c+err]:.2f}$ & ".format(r=kilo),
+			"${r[cx]:+.2f}\pm{r[cxerr]:.2f}$ \\".format(r=kilo),
+			]
+			
+		print "LaTeX table line:"
+		print "".join(ret)
+	
+	return returndict
+	
+	
+	
+	
+	
+	
+
