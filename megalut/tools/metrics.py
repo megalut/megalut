ef415f6a7ed24f4fbe260dc58476a9a84f6903ef
mtewes <malte.tewes@gmail.com>
2017-10-23 15:36:54 +0200

    Found this, not sure if useful

--- megalut/tools/metrics.py
+++ megalut/tools/metrics.py
@@ -12,6 +12,8 @@ try:
 except:
 	pass
 
+from scipy.optimize import curve_fit
+
 import logging
 logger = logging.getLogger(__name__)
 
@@ -62,11 +64,40 @@ def metrics(catalog, labelfeature, predlabelfeature, pre_is_res=False):
 	
 	return ret
 	
+def linregress_with_errors(x, y, y_err):
+    """
+	Test for comparision, GSL's gsl_fit_wlinear function
+	"""
+    
+    y_weights = y_err**-2
+    total_weight = y_weights.sum()
+    
+    x_weighted_mean = np.average(x,weights=y_weights)
+    y_weighted_mean = np.average(y,weights=y_weights)
+    
+    dx = x-x_weighted_mean
+    dy = y-y_weighted_mean
+    
+    dx2_weighted_mean = np.average(dx**2,weights=y_weights)
+    dxdy_weighted_mean = np.average(dx*dy,weights=y_weights)
+    
+    slope = dxdy_weighted_mean / dx2_weighted_mean
+    intercept = y_weighted_mean - x_weighted_mean*slope
+    
+    slope_err = np.sqrt(1./(total_weight*dx2_weighted_mean))
+    intercept_err = np.sqrt((1.0 + x_weighted_mean**2 / dx2_weighted_mean) / total_weight)
+    slope_intercept_covar = -x_weighted_mean / (total_weight*dx2_weighted_mean)
+
+    return slope, intercept, slope_err, intercept_err, slope_intercept_covar
+
 
 def wmetrics(cat, trufeat, predfeat, wfeat=None):
 	"""
 	An attempt to get metrics with errors when one has weights, *without* first "bining" things.
 	Warning, this is experimental, not yet fully tested.
+	
+	Note that the scale of weights matters for the errors!
+	Provide weights corresponding to 1/sigma**2
 	"""
 	logger.info("Computing metrics for {} -> {}".format(predfeat.colname, trufeat.colname))
 	
@@ -83,15 +114,33 @@ def wmetrics(cat, trufeat, predfeat, wfeat=None):
 		x = metcat[trufeat.colname]
 		y = metcat[predfeat.colname]
 		w = metcat[wfeat.colname]
-	print x
-	print y
-	print w
 	
+
+	y_err = np.sqrt(1.0/np.clip(w, 1e-18, 1e18)) # We don't want weights of exactly zero here.
+	
+	########### GSL
+	(slope, intercept, slope_err, intercept_err, slope_intercept_covar) = linregress_with_errors(x, y, y_err)
+	txt = "m*1e3: %.1f +/- %.1f   c*1e3: %.1f +/- %.1f" % ((slope-1.0)*1000.0, slope_err*1000.0, intercept*1000.0, intercept_err*1000.0)
+	logger.info("GSL regression: {}".format(txt))	
+	
+
+	########### Scipy
+	def f(x, a, b):
+		return a * x + b
+
+	p0 = [0, 0] # initial parameter estimate
+	popt, pcov = curve_fit(f, x, y, p0, y_err, absolute_sigma=True)
+	perr = np.sqrt(np.diag(pcov))
+	txt = "m*1e3: %.1f +/- %.1f   c*1e3: %.1f +/- %.1f" % ((popt[0]-1.0)*1000.0, perr[0]*1000.0, popt[1]*1000.0, perr[1]*1000.0)
+	logger.info("Scipy regression: {}".format(txt))	
+	
+	
+	########### statsmodels
 	X = sm.add_constant(x)
 	#model = sm.OLS(y, X)
-	model = sm.WLS(y, X, w=1.0/w)
+	model = sm.WLS(y, X, weights=1.0/(y_err**2)) # Yes, this "w" should be proportional to the inverse of the variance
 	
-	results = model.fit()
+	results = model.fit(cov_type='fixed scale')
 	print(results.summary())
 	
 	print('Parameters: ', results.params)
@@ -104,7 +153,7 @@ def wmetrics(cat, trufeat, predfeat, wfeat=None):
 	ret = {"m":m, "c":c, "merr":merr, "cerr":cerr}
 	
 	txt = "m*1e3: %.1f +/- %.1f   c*1e3: %.1f +/- %.1f" % (m*1000.0, merr*1000.0, c*1000.0, cerr*1000.0)
-	logger.info("Regression: {}".format(txt))	
+	logger.info("StatsModels Regression: {}".format(txt))	
 	
 	return ret
 	
