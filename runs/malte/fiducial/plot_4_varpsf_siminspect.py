e314bf02d055edd013090d4d29900a8c1b10aa82
mtewes <malte.tewes@gmail.com>
2018-02-28 17:32:37 +0100

    new plot to check var psf

--- runs/malte/fiducial/plot_4_varpsf_siminspect.py
+++ runs/malte/fiducial/plot_4_varpsf_siminspect.py
@@ -1,81 +1,41 @@
-"""
-Plots the figure of the paper 
-"""
-
-import measfcts
+import megalut
 import os
-import numpy as np
-import matplotlib.ticker as ticker
-
 import config
+import numpy as np
 
-import megalut.plot
 from megalut.tools.feature import Feature
+
+import matplotlib
 import matplotlib.pyplot as plt
 
-import logging
-logger = logging.getLogger(__name__)
-
-valname = "{}_on_{}".format(config.datasets["ts"], config.datasets["vs"])
-valcat = os.path.join(config.valdir, valname + ".pkl")
-cat = megalut.tools.io.readpickle(valcat)
 
 
-megalut.plot.figures.set_fancy(14)
-
-"""
-megalut.tools.table.addstats(cat, "snr")
-s = megalut.tools.table.Selector("ok", [
-	#("min", "snr_mean", 1.8),
-	("min", "snr_mean", 31.),
-	#("min", "tru_rad", 2.5),
-	("max", "adamom_frac", 0.01)
-	]
-	)
-
-cat = s.select(cat)
-"""
+cat = megalut.tools.io.readpickle(os.path.join(config.simmeasdir, config.datasets["si"], "groupmeascat.pkl"))
 
 print megalut.tools.table.info(cat)
 
-
-for comp in ["1", "2"]:
-	megalut.tools.table.addrmsd(cat, "pre_s{}".format(comp), "tru_s{}".format(comp))
-
-resr = 0.015
-symthres = 0.002
-
-#=======================================================================================
-fig = plt.figure(figsize=(8, 3.5))
-
-ax = fig.add_subplot(1, 2, 1)
-megalut.plot.scatter.scatter(ax, cat, Feature("tru_s1", nicename="True shear $g_1$"), Feature("pre_s1_bias", -resr, resr, nicename="Shear bias"), showidline=True, yisres=True)
-ax.fill_between([-1, 1], -symthres, symthres, alpha=0.2, facecolor='darkgrey')
-
-metrics = megalut.tools.metrics.metrics(cat,  Feature("tru_s1"),  Feature("pre_s1_bias"), pre_is_res=True)
-
-ax.annotate(r"$\mathrm{RMSD=%.5f}$" % metrics["rmsd"], xy=(0.0, 1.0), xycoords='axes fraction', xytext=(8, -4), textcoords='offset points', ha='left', va='top')
-ax.annotate(r"$10^3\mu=%.1f \pm %.1f$" % (metrics["m"]*1000.0, metrics["merr"]*1000.0), xy=(0.0, 1.0), xycoords='axes fraction', xytext=(8, -19), textcoords='offset points', ha='left', va='top')
-ax.annotate(r"$10^3c=%.1f \pm %.1f$" % \
-	(metrics["c"]*1000.0, metrics["cerr"]*1000.0), xy=(0.0, 1.0), xycoords='axes fraction', xytext=(8, -35), textcoords='offset points', ha='left', va='top')
+fig = plt.figure(figsize=(20, 13))
 
 
-ax = fig.add_subplot(1, 2, 2)
-megalut.plot.scatter.scatter(ax, cat, Feature("tru_s2", nicename="True shear $g_2$"), Feature("pre_s2_bias", -resr, resr, nicename="Shear bias"), showidline=True, yisres=True)
-ax.fill_between([-1, 1], -symthres, symthres, alpha=0.2, facecolor='darkgrey')
+ax = fig.add_subplot(2, 3, 1)
+megalut.plot.scatter.scatter(ax, cat, Feature("tru_psf_x"),  Feature("tru_psf_y"),  Feature("tru_psf_g1"))
+ax = fig.add_subplot(2, 3, 2)
+megalut.plot.scatter.scatter(ax, cat, Feature("tru_psf_x"),  Feature("tru_psf_y"),  Feature("tru_psf_g2"))
+ax = fig.add_subplot(2, 3, 3)
+megalut.plot.scatter.scatter(ax, cat, Feature("tru_psf_x"),  Feature("tru_psf_y"),  Feature("tru_psf_sigma"))
 
-metrics = megalut.tools.metrics.metrics(cat,  Feature("tru_s2"),  Feature("pre_s2_bias"), pre_is_res=True)
 
-ax.annotate(r"$\mathrm{RMSD=%.5f}$" % metrics["rmsd"], xy=(0.0, 1.0), xycoords='axes fraction', xytext=(8, -4), textcoords='offset points', ha='left', va='top')
-ax.annotate(r"$10^3\mu=%.1f \pm %.1f$" % (metrics["m"]*1000.0, metrics["merr"]*1000.0), xy=(0.0, 1.0), xycoords='axes fraction', xytext=(8, -19), textcoords='offset points', ha='left', va='top')
-ax.annotate(r"$10^3c=%.1f \pm %.1f$" % \
-	(metrics["c"]*1000.0, metrics["cerr"]*1000.0), xy=(0.0, 1.0), xycoords='axes fraction', xytext=(8, -35), textcoords='offset points', ha='left', va='top')
 
-ax.set_ylabel("")
-ax.set_yticklabels([])
+ax = fig.add_subplot(2, 3, 4)
+megalut.plot.scatter.scatter(ax, cat, Feature("tru_psf_x"), Feature("tru_psf_y"), Feature("adamom_g1"))
+ax = fig.add_subplot(2, 3, 5)
+megalut.plot.scatter.scatter(ax, cat, Feature("tru_psf_x"), Feature("tru_psf_y"), Feature("adamom_g2"))
+ax = fig.add_subplot(2, 3, 6)
+megalut.plot.scatter.scatter(ax, cat, Feature("tru_psf_x"), Feature("tru_psf_y"), Feature("adamom_sigma"))
+
 
 plt.tight_layout()
-
-megalut.plot.figures.savefig(os.path.join(config.valdir, valname + "plot_4"), fig, fancy=True, pdf_transparence=True)
 plt.show()
+plt.close(fig) # Helps releasing memory when calling in large loops.
+
 
